13240795b59962b8f3186f312246cfaa27696b0c
Bayu Candra <110009488+Bayu-x3@users.noreply.github.com>
2023-02-20 21:11:25 +0700

    Update chall2.php

--- challenge/forensic/chall2.php
+++ challenge/forensic/chall2.php
@@ -1,31 +1,67 @@
 <?php
-  include '../headF.php';
+session_start();
+include '../../assets/functions.php';
+$isLoggedIn = isset($_SESSION['username']);
+if (!$isLoggedIn) {
+    header('Location: login.php');
+}
+$username = $_SESSION['username'];
+$result = mysqli_query($conn, "SELECT * FROM tbl_users WHERE username = '$username'");
+$row = mysqli_fetch_assoc($result);
+$correctF_2 = $row['correctF_2'];
+?>
+<!-- form input jawaban -->
+<?php
+require '../headF.php';
 ?>
 <div class="card text-center">
   <div class="card-header">
-    <h1 style="color: black;" weight="bold;">Gambar</h1>
+  <h1 style="color: black;" weight="bold;">Gambar</h1>
     <h5 style="color: black;">kalau kata guru sih, di setiap gambar pasti ada kata tersembunyi nya, tapi aku gatau deh kata kata nya apaan</h5>
-    <a href="https://l.top4top.io/p_2553yibo71.png"><h5>Click here</a></h5>
+    <a href="https://l.top4top.io/p_2553yibo71.png" target="_blank"><h5>Click here</a></h5>
   </div>
   <div class="card-body">
 <form method="post">
-					<input type="text" id="chall3" class="form-control" required="required" placeholder="FLGx-3{flagnya}">
+					<input type="text" id="f2" name="f2" class="form-control" required="required" placeholder="FLGx-3{flagnya}">
 							<center><br>
-                            <input type="button" value="Check Flag" onclick="c3()" class='btn btn-success'><span id="c3"></span></strong></p>
+                            <button type="submit" name="submit" value="Check Flag" class='btn btn-success'>Check Flag</button>
 		</center>		
 		</form>
   </div>
-  <script type="text/javascript">
-      function c3() {
-        var a = document.getElementById("chall3");
-        if ((a.value == "FLGx-3{pesan_ajaib}") || (a.value == "{pesan_ajaib}")) {
-        document.getElementById('c3').innerHTML = swal('Flag Correct!', 'Go to next level', 'success')
+    </div>
+    <?php
+    if ($correctF_2 == 1) {
+        echo "<script>
+        swal('You have answered!!', 'What the fck r u doing bro?!!!', 'error');
+        </script>";
     } else {
-        document.getElementById('c3').innerHTML = swal('Flag Incorrect!!', 'Try Again!', 'error')
+        if (isset($_POST['submit'])) {
+            // baca jawaban dari form input
+            $jawaban = $_POST["f2"];
+            // cek apakah jawaban yang diberikan user benar atau salah
+            if ($jawaban == "FLGx-3{pesan_ajaib}") {
+                // jawaban benar, tambahkan 5 poin ke database dan update status jawaban user menjadi 1
+                $query = "UPDATE tbl_users SET poin = poin + 10, correctF_2 = 1
+                WHERE username = '$username'";
+                if ($conn->query($query) === TRUE) {
+                    echo "<script>
+                    swal('Correct Flag!', 'Point success add', 'success');
+                    </script>";
+                } else {
+                    echo "Error: " . $query . "<br>" . $conn->error;
+                }
+            } else {
+                // jawaban salah, tampilkan pesan "Salah"
+                echo "<script>
+                swal('Wrong Flag!!', 'Enter a correct Flag!!!', 'error');
+                </script>";
+            }
+        }
     }
-      }
-      </script>
-  <br><br>
-<?php
-	include '../../footer.php';
-?>
+    // tutup koneksi ke database
+    $conn->close();
+    ?>
+    <br><br>
+    <?php
+    include '../../assets/footer.php';
+    ?>
