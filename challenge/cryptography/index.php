0b22d8379c52d04cfe620c6da59ac0bb84b1d1da
Bayu Candra <110009488+Bayu-x3@users.noreply.github.com>
2023-02-20 21:14:51 +0700

    Update index.php

--- challenge/cryptography/index.php
+++ challenge/cryptography/index.php
@@ -1,14 +1,21 @@
 <?php
-    include '../../headChall.php';
+session_start();
+include '../../assets/functions.php';
+$isLoggedIn = isset($_SESSION['username']);
+if(!$isLoggedIn){
+    header('Location:../../login.php');
+}
 ?>  
-        <hr>
+<?php
+ include '../../assets/headChall.php';
+ ?>
         <b><center><h1>CRYPTOGRAPHY CHALLENGE</h1></center></b>
         <br>
         <div class="contentadmin">
             <div>                
-               <a href="/challenge/cryptography/chall1.php" style="text-decoration: none;"><button class="btn-content">Level 1</button></a>
-               <a href="/challenge/cryptography/chall2.php" style="text-decoration: none;"><button class="btn-content">Level 2</button></a>
-               <a href="/challenge/cryptography/chall3.php" style="text-decoration: none;"><button class="btn-content">Level 3</button></a>
+               <a href="../cryptography/chall1.php" style="text-decoration: none;"><button class="btn-content">Level 1 (5 Point)</button></a>
+               <a href="../cryptography/chall2.php" style="text-decoration: none;"><button class="btn-content">Level 2 (10 Point)</button></a>
+               <a href="../cryptography/chall3.php" style="text-decoration: none;"><button class="btn-content">Level 3 (15 Point)</button></a>
         </div>        
         <br>
             <br>
