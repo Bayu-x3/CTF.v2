3e6ad619c93eb8f9645351587f262d35bcafb885
Bayu Candra <110009488+Bayu-x3@users.noreply.github.com>
2023-02-20 21:09:32 +0700

    Update chall2.php

--- challenge/websec/chall2.php
+++ challenge/websec/chall2.php
@@ -1,31 +1,65 @@
 <?php
-  include '../headM.php';
+session_start();
+include '../../assets/functions.php';
+$isLoggedIn = isset($_SESSION['username']);
+if (!$isLoggedIn) {
+    header('Location: login.php');
+}
+$username = $_SESSION['username'];
+$result = mysqli_query($conn, "SELECT * FROM tbl_users WHERE username = '$username'");
+$row = mysqli_fetch_assoc($result);
+$correctW_2 = $row['correctW_2'];
+?>
+<!-- form input jawaban -->
+<?php
+require '../headW.php';
 ?>
 <div class="card text-center">
   <div class="card-header">
     <h1 style="color: black;">Form login</h1>
     <h5 style="color: black;">Hai semua nama aku adalah Default, aku adalah seorang ADMIN di salah satu grub facebook yang berjudul WORDPRESS</h5>
-    <a href="form.php"><h5>Click here</a></h5>
   </div>
   <div class="card-body">
 <form method="post">
-					<input type="text" id="chall1" class="form-control" required="required" placeholder="FLGx-3{flagnya}">
+					<input type="text" id="w2" name="w2" class="form-control" required="required" placeholder="FLGx-3{flagnya}">
 							<center><br>
-                            <input type="button" value="Check Flag" onclick="c1()" class='btn btn-success'><span id="c1"></span></strong></p>
+                            <button type="submit" name="submit" value="Check Flag" class='btn btn-success'>Check Flag</button>
 		</center>		
 		</form>
-  </div>
-  <script type="text/javascript">
-      function c1() {
-        var a = document.getElementById("chall1");
-        if ((a.value == "FLGx-3{wordpress_def}") || (a.value == "wordpress_def")) {
-        document.getElementById('c1').innerHTML = swal('Flag Correct!', 'Go to next level', 'success')
+    </div>
+    <?php
+    if ($correctW_2 == 1) {
+        echo "<script>
+        swal('You have answered!!', 'What the fck r u doing bro?!!!', 'error');
+        </script>";
     } else {
-        document.getElementById('c1').innerHTML = swal('Flag Incorrect!!', 'Silahkan masukan flag dan format yang benar', 'error')
+        if (isset($_POST['submit'])) {
+            // baca jawaban dari form input
+            $jawaban = $_POST["w2"];
+            // cek apakah jawaban yang diberikan user benar atau salah
+            if ($jawaban == "FLGx-3{wordpress_def}") {
+                // jawaban benar, tambahkan 5 poin ke database dan update status jawaban user menjadi 1
+                $query = "UPDATE tbl_users SET poin = poin + 12, correctW_2 = 1
+                WHERE username = '$username'";
+                if ($conn->query($query) === TRUE) {
+                    echo "<script>
+                    swal('Correct Flag!', 'Point success add', 'success');
+                    </script>";
+                } else {
+                    echo "Error: " . $query . "<br>" . $conn->error;
+                }
+            } else {
+                // jawaban salah, tampilkan pesan "Salah"
+                echo "<script>
+                swal('Wrong Flag!!', 'Enter a correct Flag!!!', 'error');
+                </script>";
+            }
+        }
     }
-      }
-      </script>
-  <br><br>
-<?php
-	include '../../footer.php';
-?>
+    // tutup koneksi ke database
+    $conn->close();
+    ?>
+    <br><br>
+    <?php
+    include '../../assets/footer.php';
+    ?>
