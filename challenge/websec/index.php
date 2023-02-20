41f943a31629ec483190af009a506659ec7a09f6
Bayu Candra <110009488+Bayu-x3@users.noreply.github.com>
2023-02-20 21:12:44 +0700

    Update index.php

--- challenge/websec/index.php
+++ challenge/websec/index.php
@@ -1,14 +1,21 @@
 <?php
-    include '../../headChall.php';
+session_start();
+include '../../assets/functions.php';
+$isLoggedIn = isset($_SESSION['username']);
+if(!$isLoggedIn){
+    header('Location:../../login.php');
+}
 ?>  
-        <hr>
-        <b><center><h1>WEB SECURITY CHALLENGE</h1></center></b>
+<?php
+ include '../../assets/headChall.php';
+ ?>
+        <b><center><h1>WEBSEC CHALLENGE</h1></center></b>
         <br>
         <div class="contentadmin">
             <div>                
-               <a href="/challenge/websec/chall1.php" style="text-decoration: none;"><button class="btn-content">Level 1</button></a>
-               <a href="/challenge/websec/chall2.php" style="text-decoration: none;"><button class="btn-content">Level 2</button></a>
-               <a href="/challenge/websec/chall3.php" style="text-decoration: none;"><button class="btn-content">Level 3</button></a>
+               <a href="../websec/chall1.php" style="text-decoration: none;"><button class="btn-content">Level 1 (7 Point)</button></a>
+               <a href="../websec/chall2.php" style="text-decoration: none;"><button class="btn-content">Level 2 (12 Point)</button></a>
+               <a href="../websec/chall3.php" style="text-decoration: none;"><button class="btn-content">Level 3 (18 Point)</button></a>
         </div>        
         <br>
             <br>
